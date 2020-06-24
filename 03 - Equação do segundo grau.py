9b5a2780b302e9a4c37f36cc16fd0ecc69547ec2
Gabriel Reira <62738460+GabrielReira@users.noreply.github.com>
2020-06-24 20:36:40 -0300

    corrigindo erro no exercício 03

--- 03 - Equação do segundo grau.py
+++ 03 - Equação do segundo grau.py
@@ -1,9 +1,9 @@
 # PROGRAMA QUE RESOLVA UMA EQUAÇÃO QUADRÁTICA.
 
 print('EQUAÇÃO DO SEGUNDO GRAU!')
-a = int(input('Digite o número que vem acompanhado por x ao quadrado: '))
-b = int(input('Agora o que vem acompanhado somente por x: '))
-c = int(input('Agora o termo independente: '))
-r1 = int((-b+(b**2-4*a*c)**(1/2))/(2*a))
-r2 = int((-b-(b**2-4*a*c)**(1/2))/(2*a))
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
+r1 = (-b+(b**2-4*a*c)**(1/2))/(2*a)
+r2 = (-b-(b**2-4*a*c)**(1/2))/(2*a)
 print(f'As raízes desta equação são {r1} e {r2}.')
